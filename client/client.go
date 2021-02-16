8778b1192f927accbfdba4feb20eae800356041f
famesensor <sathit.ju@outlook.com>
2021-02-16 22:32:29 +0700

    :hammer: handler client conection

--- client/client.go
+++ client/client.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"context"
+	"go-grpc/proto"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	log.Println("Hello client...")
+
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("localhost:3000", opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cc.Close()
+
+	client := proto.NewHelloServiceClient(cc)
+	request := &proto.HelloRequest{Name: "Fame"}
+
+	res, err := client.Hello(context.Background(), request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Response : ", res.Greeting)
+}

1e361817605115146ca1a50777f04446390493c3
famesensor <sathit.ju@outlook.com>
2021-04-11 20:45:19 +0700

    [:hammer:]: add func. and fix func  for grpc

--- server/server.go
+++ server/server.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"context"
-	"go-grpc/proto"
+	"errors"
+	"famesensor/go-grpc-learn/proto"
+	"flag"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type server struct {
+	proto.UnimplementedHelloServiceServer
+}
+
+func (s *server) Password(ctx context.Context, request *proto.PasswordRequest) (*proto.PasswordResponse, error) {
+	if request.Password.Password != request.Password.ConfirmPassword {
+		return nil, status.Error(codes.InvalidArgument, errors.New("Error password").Error())
+	}
+
+	return &proto.PasswordResponse{
+		Status:  "success",
+		Message: "Password",
+	}, nil
 }
 
 func (s *server) Hello(ctx context.Context, request *proto.HelloRequest) (*proto.HelloReponse, error) {
@@ -22,6 +38,7 @@ func (s *server) Hello(ctx context.Context, request *proto.HelloRequest) (*proto
 }
 
 func main() {
+	flag.Parse()
 	// register server and it listen on port 3000 for grpc cpnnection
 	lis, err := net.Listen("tcp", ":3000")
 	if err != nil {
@@ -36,4 +53,5 @@ func main() {
 	if err := srv.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
+
 }
